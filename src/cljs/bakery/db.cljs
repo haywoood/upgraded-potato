dc9d233c6e7470ab98fda62092e648617617ca2d
lsdafjklsd <ryanhaywoodj@gmail.com>
2018-02-11 22:34:41 -0500

    compile to static project, add more comments

--- src/cljs/bakery/db.cljs
+++ src/cljs/bakery/db.cljs
@@ -2,6 +2,10 @@
 
 (def raw-data (js->clj js/window.data :keywordize-keys true))
 
+;; :cart is a map of key value pairs, where the key is the
+;; corresponding treat id, and the value is a number that we
+;; use to keep track of the item amount via `(update-in db [:cart id] inc)`
+
 (def default-db
   {:treats (:treats raw-data)
    :cart {}})
